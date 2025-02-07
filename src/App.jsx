697fc3de3b07e8128226e561abaa1bb497dd30ed
baldx <mr.ahmad.m2006@gmail.com>
2025-02-07 14:00:39 +0100

    add login restriction

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import './App.css'
 import LandingPage from './components/Landingpage'
 import Navbar from './components/Navbar'
+import LogIn from './components/Login';
 
 function App() {
+  const [login, setLogin] = useState(false);
 
-  return (
-    <>
-      <Navbar/>
-      <LandingPage/>
-      <aside></aside>
-    </>
-  )
+  if (login) {
+    return (
+      <>
+        <Navbar/>
+        <LandingPage/>
+        <aside></aside>
+      </>
+      )
+  } else {
+    return (
+      <>
+        <LogIn />
+      </>
+    )
+  }
+
 }
 
 export default App
