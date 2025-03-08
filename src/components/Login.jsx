c55a9b3a279f484038d63fae597326783a415f54
baldx <125921176+baldx@users.noreply.github.com>
2025-03-08 15:28:39 +0100

    import bootstrap form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,33 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css';
+
 function LogIn() {
+    
     return (
-        <>
-            <h1>Create your account!</h1>
-            <form action="">
-                <label htmlFor="email">
-                    Email
-                    <input type="text" name="email" id="name" max={20}/>
-                </label>
-                <label htmlFor="password">
-                    Password
-                    <input type="password" name="password" id="password" max={20}/>
-                </label>
-                <label htmlFor="tos">
-                    Accept <a href="">Terms of service</a>
-                <input type="checkbox" name="tos" id="tos" />
-                </label>
-            </form>
-        </>
+        <div className="log-in-form">
+            <div className="log-in-container">
+                <h1>Log in</h1>
+                <small>To use our platform, we first need you to log in!</small>
+                    <Form>
+                <Form.Group className="mb-3" >
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" />
+                    <Form.Text className="text-muted">
+                    Your username will be displayed for other users to see!
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={e => {e.preventDefault()}}>
+                    Log in
+                </Button>
+                </Form>
+            </div>
+        </div>
 
     )
 }
---
+++ src/components/Signup.jsx
@@ -0,0 +1,40 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Login.css';
+
+function Signup() {
+    
+    return (
+        <div className="log-in-form">
+            <div className="log-in-container">
+                <h1>Sign up</h1>
+                <small>To use our platform, we first need you to create an account!</small>
+                <Form>
+            <Form.Group className="mb-3" >
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Enter username" />
+                <Form.Text className="text-muted">
+                Your username will be displayed for other users to see!
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3 TOS" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Agree to the TOS"  />
+                <Form.Text><a href="../TOS" className="TOS-text">Teams of service</a></Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={e => {e.preventDefault()}}>
+                Sign up
+            </Button>
+            </Form>
+            </div>
+        </div>
+
+    )
+}
+
+
+export default Signup;
