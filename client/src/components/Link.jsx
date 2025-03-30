0c0c3a46838df25a77a73e26fa2bfbfd519cd1db
baldx <125921176+baldx@users.noreply.github.com>
2025-03-30 21:48:54 +0200

    switch to link react router dom

--- client/src/components/Link.jsx
+++ client/src/components/Link.jsx
@@ -1,13 +1,15 @@
-function Link({icon, title}) {
+import { Link } from "react-router-dom";
+
+function Title({icon, title}) {
 
     return ( //component for links. adds title and icon to the link
         <>
             <section id="tab"> 
             <i className="material-icons" id="material-icons">{icon}</i>
-                <a href="">{title}</a>
+                <Link to={title}>{title}</Link>
             </section>
         </>
     )
 }
 
-export default Link;
+export default Title;
