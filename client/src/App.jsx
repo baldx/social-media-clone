56071566c2bf5dac4c36583fecc0267cb7aae554
baldx <125921176+baldx@users.noreply.github.com>
2025-03-30 21:48:28 +0200

    remove stuff from App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,31 +7,7 @@ import './App.css'
 
 
 function App() {
-  const status = ["notSignedUp", "notLoggedIn", "Logged in"]; // options to display different parts of the webbpage
-  const [login, setLogin] = useState(status[0]); //sets the status if user is logged in
-
-  if (login === status[2]) { //if user is logged in display the page
-    return (
-      <>
-        <Navbar/>
-        <LandingPage/>
-        <aside></aside>
-      </>
-      )
-  } else if (login === status[1]) { //if not logged in display login page
-    return (
-      <>
-        <LogIn />
-      </>
-    )
-  }
-  else if (login === status[0]) { //if not signed up then display sign up page
-    return (
-      <>
-        <Signup />
-      </>
-    )
-  }
+  
 
 }
 
