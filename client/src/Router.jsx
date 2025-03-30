9d796f85eb0d55cbddeaa993a55f59e8c865853c
baldx <125921176+baldx@users.noreply.github.com>
2025-03-30 21:48:40 +0200

    add routing

---
+++ client/src/Router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPage from './components/Landingpage'; // The main page with Stories and Link components
+import Login from './components/Login'; // Login Component
+import Signup from './components/Signup'; // Signup Component
+import Navbar from './components/Navbar'; // Common Navbar Component
+import ErrorPage from './components/ErrorPage'; // Error Page Component
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Navbar /> {/* navbar is always visible */}
+          <LandingPage /> {/* landingPage component with Stories and Link */}
+        </>
+      ),
+      errorElement: <ErrorPage />, //adds error page if something went wrong
+    },
+    {
+        path: "/login", //adds login path
+        element: <Login />,
+    },
+    {
+        path: "/signup",//adds signup path
+        element: <Signup />
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import Router from './Router.jsx';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -10,6 +9,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
