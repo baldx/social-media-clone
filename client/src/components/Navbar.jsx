8ed83b2fc288f27dcd6b107bfbcc08cd8aacb418
baldx <125921176+baldx@users.noreply.github.com>
2025-03-30 21:49:06 +0200

    switch name

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import Link from './Link.jsx';
+import Title from './Link.jsx';
 import ElderBond from '../assets/ElderBondLogo.png';
 
 function Navbar() {
@@ -7,13 +7,13 @@ function Navbar() {
         <nav>
             <div id="title">Elder bond</div>
             
-            <Link icon={"home"} title={"Home"} />
+            <Title icon={"home"} title={"Home"} />
 
-            <Link icon={"explore"} title={"Explore"} />
+            <Title icon={"explore"} title={"Explore"} />
 
-            <Link icon={"chat_bubble"} title={"Messages"} />
+            <Title icon={"chat_bubble"} title={"Messages"} />
 
-            <Link icon={"account_circle"} title={"Profile"} />
+            <Title icon={"account_circle"} title={"Profile"} />
         </nav>
         </>
     )
